8dfc84e91008aa782051f6d735f57483afc60312
dorachenxiW <Dora.Wang@lincolnuni.ac.nz>
2024-04-08 12:46:48 +1200

    login and signup working

--- pokeno-piano-studio/back_end/server.js
+++ pokeno-piano-studio/back_end/server.js
@@ -1,9 +1,16 @@
-const express = require('express');
-const mysql = require('mysql');
-const cors = require('cors');
+import express, { response } from 'express';
+import mysql from 'mysql';
+import cors from 'cors';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import cookieParser from 'cookie-parser';
+
+const salt = 10;
 
 const app = express();
+app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 
 const db = mysql.createConnection({
     host: 'localhost',
@@ -17,7 +24,7 @@ app.get('/', (re,res) => {
 })
 
 app.get('/users', (req,res) => {
-    const sql = "SELECT * FROM user";
+    const sql = "SELECT * FROM student";
     db.query(sql, (err, data) => {
         if (err) return res.json(err);
         return res.json(data);
@@ -29,6 +36,43 @@ app.get('/pricing', (req,res) =>{
     db.query(sql,(err, data) => {
         if (err) return res.json(err);
         return res.json(data);
+    })
+})
+
+app.post('/signup', (req, res) => {
+    const sql = "INSERT INTO student (`first_name`,`last_name`,`email`,`password`) VALUES (?)";
+    bcrypt.hash(req.body.password.toString(), salt, (err, hash) => {
+        if (err) return res.json({error: "Error for hashing password."})
+        const values = [
+            req.body.first_name,
+            req.body.last_name,
+            req.body.email,
+            hash
+        ]
+        db.query(sql, [values], (err, result) => {
+            if (err) return res.json({Eroor: "Inserting data error in server"});
+            return res.json ({Status: "Success"});
+        })
+    })
+})
+
+app.post('/login', (req, res) => {
+    const sql = "SELECT * FROM student WHERE email = ?";
+    db.query (sql, [req.body.email], (err, data) => {
+        if (err) return res.json({error: "Login error in server"})
+        if (data.length > 0 ) {
+            bcrypt.compare(req.body.password.toString(), data[0].password, (err, response) => {
+                if (err) return res.json({Error: "Password commpare error"});
+                if (response) {
+                    return res.json ({Status: "Success"});
+                } else {
+                    return res.json ({Error: "Password not matched"});
+                }
+            })
+
+        } else {
+            return res.json({Error: "No email exsited"});
+        }
     })
 })
 
--- pokeno-piano-studio/src/GetStarted.js
+++ pokeno-piano-studio/src/GetStarted.js
@@ -1,8 +1,72 @@
+import { useState } from "react";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
+
 const GetStarted = () => {
+    const [values, setValues] = useState ({
+        first_name: '',
+        last_name: '',
+        email: '',
+        password: ''
+    })
+    const history = useHistory()
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        axios.post("http://localhost:5000/signup", values)
+        .then(res => {
+            if (res.data.Status === "Success") {
+                history.push('/login')
+            } else {
+                alert("Error");
+            }
+        })
+        .then(err => console.log(err));
+    }
     return ( 
-        <div className="signup">
-            <h2>This is the sign up page</h2>
-        </div>
+            <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-md-6">
+                    <div className="card mt-5">
+                        <div className="card-body">
+                            <h2 className="card-title">Create your account</h2>
+                            <p className="card-text">Please enter your details below and we'll create your account.</p>
+                            <form onSubmit={handleSubmit}> 
+                                <div className="form-group">
+                                    <label htmlFor="first_name">First name</label>
+                                    <input type="text" className="form-control" id="first_name" name="first_name" 
+                                    onChange = {e => setValues({...values, first_name: e.target.value})} 
+                                    required />
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="last_name">Last name</label>
+                                    <input type="text" className="form-control" id="last_name" name="last_name" 
+                                    onChange = {e => setValues({...values, last_name: e.target.value})}
+                                    required />
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="email">Email address</label>
+                                    <input type="email" className="form-control" id="email" name="email" 
+                                    onChange = {e => setValues({...values, email: e.target.value})}
+                                    required />
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="password">Password</label>
+                                    <input type="password" className="form-control" id="password" name="password"  minLength="5"
+                                    onChange = {e => setValues({...values, password: e.target.value})}
+                                    required />
+                                </div>
+                                <p>
+                                Note: Password must be at least 5 characters long. 
+                                </p>
+                                <p></p>
+                                <button type="submit" className="custom-button-color">Create Account</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>     
      );
 }
  
