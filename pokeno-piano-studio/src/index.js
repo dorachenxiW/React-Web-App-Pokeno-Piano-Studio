bae0bf818741f7fe8763f9b4cfb410d332d854e2
dorachenxiW <Dora.Wang@lincolnuni.ac.nz>
2024-04-27 12:52:30 +1200

    logout address jump worked

--- pokeno-piano-studio/src/index.js
+++ pokeno-piano-studio/src/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom'; // Import BrowserRouter
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router> {/* Wrap your App component with Router */}
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
