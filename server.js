9b65771a1550823f038650eff2738057f1e5a7ae
dorachenxiW <Dora.Wang@lincolnuni.ac.nz>
2024-05-30 19:37:42 +1200

    make it better

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import cookieParser from 'cookie-parser';
 
 const salt = 10;
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(express.json());
